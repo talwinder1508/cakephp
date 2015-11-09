96f92b88c248f47e60a66995958a9b93e396561a
Talwinder <Talwinder1508@live.com>
2015-11-09 13:00:39 -0500

    created articles

---
+++ src/Controller/ArticlesController.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Controller;
+
+class ArticlesController extends AppController
+{
+
+    public function index()
+    {
+         $this->set('articles', $this->Articles->find('all'));
+    }
+
+    public function view($id = null)
+    {
+        $article = $this->Articles->get($id);
+        $this->set(compact('article'));
+    }
+}
+
+?>
